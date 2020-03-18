f847643a90e2190d361632f01924b683a1230c03
Vitaly1605 <Vitaly_1605@tut.by>
2020-03-19 01:09:34 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    return String(n)
+        .split("")
+        .reverse()
+        .join("");
+};
